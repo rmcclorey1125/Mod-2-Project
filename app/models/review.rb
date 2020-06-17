332bc9e22d7b6ed84f73e584c16bab0c80609d95
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-06-17 17:36:02 -0400

    css styling

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
 
-    def review_show
-        "#{self.user.username}\nRating: #{self.rating}\nReview:#{content}"
-    end
+    # def review_show
+    #     "#{self.user.username}\nRating: #{self.rating}\nReview:#{content}"
+    # end
 
 end
