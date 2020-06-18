1da4b281421cd69a5a981ab82b7be4c80003dc82
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-06-18 11:09:22 -0400

    follow button done

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,9 @@
+class FollowsController < ApplicationController
+
+    def create 
+       # @user = User.find(params[:id])
+        byebug
+        # Follow.create(follower_id: @current_user.id, followee_id: )
+    end 
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   delete "sessions/logout", to: "sessions#logout", as: "logout" 
   get "sessions/new", to: "sessions#new", as: "new_login"
   post "/sessions", to: "sessions#create", as: "login"
+  post "users/follow", to: "users#follow", as: "follow"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
